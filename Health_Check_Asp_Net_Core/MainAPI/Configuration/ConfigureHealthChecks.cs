61f6b949eb0f5f79880b34bcb321fc2781465f3b
Vicluar Daniel Merchan <vicluar@gmail.com>
2020-05-24 15:54:29 +0200

    Moving my health check custom to its own class.

---
+++ Health_Check_Asp_Net_Core/MainAPI/Configuration/ConfigureHealthChecks.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MainAPI.Configuration
+{
+    public static class ConfigureHealthChecks
+    {
+        public static IServiceCollection AddCustomHealthChecks(this IServiceCollection services)
+        {
+            services.AddHealthChecks();
+
+            return services;
+        }
+    }
+}
